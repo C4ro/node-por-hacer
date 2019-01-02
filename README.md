3d3f3c07f0f16c72819dc06c97b596bd24df8461
C4ro <carolina.sanmartin09@gmail.com>
2019-01-02 16:40:25 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación de comandos en node
+
+Aplicación que permite la gestión de una lista de cosas por hacer
+
+Instalar paquetes de node con
+
+```
+npm install
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer.splice(index, 1);
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+const guardarDB = () => {
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) {
+            throw new Error('No se pudo grabar');
+        }
+    })
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
